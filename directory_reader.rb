08612751d207b167e790199bb91c6e02db17ab98
shihaotan91 <shihaotan91@gmail.com>
2019-06-23 13:05:06 +0800

    WIP - write instance methods to find file paths with todos in them

--- directory_reader.rb
+++ directory_reader.rb
@@ -1,8 +1,22 @@
 class DirectoryReader
   def initialize(path)
     path = "#{path}/**/*"
-    directory = Dir[path]
+    @directory = Dir[path]
+  end
+
+  def all_file_paths
+    @directory.select do |path|
+      File.file? path
+    end
+  end
+
+  def todo_file_paths
+    all_file_paths.select do |file|
+      data_as_string = File.read(file)
+      data_as_string.upcase.include? "TODO"
+    end
   end
 end
 
-DirectoryReader.new('test_dir')
+new_directory = DirectoryReader.new('test_dir')
+puts new_directory.todo_file_paths
