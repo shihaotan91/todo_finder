53e74a35909f76c46b997d052acfc0bead10599c
shihaotan91 <shihaotan91@gmail.com>
2019-06-23 21:34:40 +0800

    WIP - refactor DirectoryReader class and update specs according to refactor

--- directory_reader.rb
+++ directory_reader.rb
@@ -1,32 +1,38 @@
 require "byebug"
 
 class DirectoryReader
-  def initialize(path)
-    path = "#{path}/**/*"
+  attr_reader :directory
+
+  def initialize(dir)
+    path = "#{dir}/**/*"
     @directory = Dir[path]
   end
 
-  def all_file_paths
+  def file_paths
     @directory.select do |path|
       File.file? path
     end
   end
 
-  def multi_line_comment(line)
+  def multi_line_comment?(line)
     line.start_with?('/*', '*/')
   end
 
+  def file_has_todo(is_multi_line_comment, line)
+    if is_multi_line_comment
+      line.include?('TODO:')
+    else
+      line.start_with?('//') && line.include?('TODO:')
+    end
+  end
+
   def todo_file_paths
-    all_file_paths.select do |file|
+    file_paths.select do |file|
       data_in_lines = File.readlines(file)
       multi_line_comment = false
       valid_lines = data_in_lines.select do |line|
-        multi_line_comment = !multi_line_comment if multi_line_comment(line)
-        if multi_line_comment
-          line.include?('TODO:')
-        else
-          line.start_with?('//') && line.include?('TODO:')
-        end
+        multi_line_comment = !multi_line_comment if multi_line_comment?(line)
+        file_has_todo(multi_line_comment, line)
       end
 
       valid_lines.length.positive?
--- directory_reader_spec.rb
+++ directory_reader_spec.rb
@@ -3,9 +3,15 @@ require "test/unit"
 require "byebug"
 
 class DirectoryReaderTest < Test::Unit::TestCase
-  def test_a
-    path = 'test_dir'
-    @@directory_reader = DirectoryReader.new("#{path}/**/*")
+  def test_config
+    dir = 'test_dir'
+    @@directory_reader = DirectoryReader.new(dir)
+  end
+
+  def test_directory
+    path = "test_dir/**/*"
+    all_files_and_dirs = Dir[path]
+    assert_equal(all_files_and_dirs, @@directory_reader.directory)
   end
 
   def test_files_with_todos
@@ -13,19 +19,25 @@ class DirectoryReaderTest < Test::Unit::TestCase
     assert_equal(paths_with_todos, @@directory_reader.todo_file_paths.sort)
   end
 
-  def test_all_file_paths
+  def test_file_paths
     all_paths = ['test_dir/dir2/module2/test4.js',
                  'test_dir/dir2/test2.js',
                  'test_dir/dir3/test5.js',
                  'test_dir/dir1/test1.js',
                  'test_dir/dir1/module1/test3.js'].sort
 
-    assert_equal(all_paths, @@directory_reader.all_file_paths.sort)
+    assert_equal(all_paths, @@directory_reader.file_paths.sort)
   end
 
   def test_multi_line_comment
-    assert_equal(true, @@directory_reader.multi_line_comment('/*'))
-    assert_equal(true, @@directory_reader.multi_line_comment('/*'))
-    assert_equal(false, @@directory_reader.multi_line_comment('-/-'))
+    assert_equal(true, @@directory_reader.multi_line_comment?('/*'))
+    assert_equal(true, @@directory_reader.multi_line_comment?('/*'))
+    assert_equal(false, @@directory_reader.multi_line_comment?('-/-'))
+  end
+
+  def test_file_has_todo
+    assert_equal(true, @@directory_reader.file_has_todo(true, 'TODO:'))
+    assert_equal(true, @@directory_reader.file_has_todo(false, '// TODO:'))
+    assert_equal(false, @@directory_reader.file_has_todo(false, 'TODO:'))
   end
 end
